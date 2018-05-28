4ee1d3273d0747e3e3e7505ad0bc12f54364b129
Chaeeun Han <channiy22@likelion.org>
2018-05-28 11:52:28 +0000

    upload

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+#Note
   # Create
   get '/notes/new'
-  get '/notes/create' => 'notes#create'
+  post '/notes' => 'notes#create'
   # post '/notes' => 'notes#create'
   
   # Read
@@ -9,11 +10,17 @@ Rails.application.routes.draw do
   get '/notes/:id' => 'notes#show'
   
   # Update
-  get 'notes/edit/:id' => 'notes#edit'
-  get 'notes/update/:id' => 'notes#update'
+  get '/notes/edit/:id' => 'notes#edit'
+  patch '/notes/:id' => 'notes#update'
   # patch 'notes/:id' => 'notes#update'
   
   # Destory
-  get '/notes/destroy/:id' => 'notes#destroy'
+  delete '/notes/:id' => 'notes#destroy'
   # delete '/notes/:id' => 'notes#destroy'
+
+# Comments
+  #Create
+  post '/comments' => 'comments#create'
+  #Delete
+  delete '/comments/:id' => 'comments#destroy'
 end
